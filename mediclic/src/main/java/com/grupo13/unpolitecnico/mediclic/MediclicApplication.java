ed0fb9192353a16cc4edd142e8474d5178c6217c
jennyZam <91443477+jennyZam@users.noreply.github.com>
2022-08-23 10:30:50 -0500

    Commit inicial
    Creacion de proyecto para software MediClic

---
+++ mediclic/src/main/java/com/grupo13/unpolitecnico/mediclic/MediclicApplication.java
@@ -0,0 +1,13 @@
+package com.grupo13.unpolitecnico.mediclic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediclicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediclicApplication.class, args);
+	}
+
+}
